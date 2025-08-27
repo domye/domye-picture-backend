b76e4833850aff95e2852d7fb5f31be1e63a1aed
Domye <a1523610551@163.com>
2025-08-27 12:46:14 +0800

    完善用户接口

--- src/main/java/com/domye/picture/service/UserService.java
+++ src/main/java/com/domye/picture/service/UserService.java
@@ -1,7 +1,14 @@
 package com.domye.picture.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.domye.picture.model.dto.user.UserQueryRequest;
 import com.domye.picture.model.entity.User;
+import com.domye.picture.model.vo.LoginUserVO;
+import com.domye.picture.model.vo.UserVO;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @author Domye
@@ -9,5 +16,29 @@ import com.domye.picture.model.entity.User;
  * @createDate 2025-08-26 15:00:43
  */
 public interface UserService extends IService<User> {
+    Long UserRegister(String userAccount, String password, String checkPassword);
 
+    /**
+     * 用户登录
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    LoginUserVO getLoginUserVO(User user);
+
+    User getLoginUser(HttpServletRequest request);
+
+    boolean userLogout(HttpServletRequest request);
+
+    public UserVO getUserVO(User user);
+
+    public List<UserVO> getUserVOList(List<User> userList);
+
+    public QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+    String getEncryptPassword(String defaultPassword);
 }
+
--- src/main/java/com/domye/picture/service/impl/UserServiceImpl.java
+++ src/main/java/com/domye/picture/service/impl/UserServiceImpl.java
@@ -1,10 +1,29 @@
 package com.domye.picture.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.domye.picture.exception.BusinessException;
+import com.domye.picture.exception.ErrorCode;
 import com.domye.picture.mapper.UserMapper;
+import com.domye.picture.model.dto.user.UserQueryRequest;
 import com.domye.picture.model.entity.User;
+import com.domye.picture.model.enums.UserRoleEnum;
+import com.domye.picture.model.vo.LoginUserVO;
+import com.domye.picture.model.vo.UserVO;
 import com.domye.picture.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.domye.picture.constant.UserConstant.USER_LOGIN_STATE;
 
 /**
  * @author Domye
@@ -15,6 +34,147 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         implements UserService {
 
+    @Override
+    public Long UserRegister(String userAccount, String password, String checkPassword) {
+        //1. 检验参数是否合法
+        if (StrUtil.hasBlank(userAccount, password, checkPassword))
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数不能为空");
+        if (userAccount.length() < 6 || userAccount.length() > 16)
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号长度必须在6-16位之间");
+        if (password.length() < 6 || password.length() > 16)
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码长度必须在6-16位之间");
+        if (!password.equals(checkPassword))
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "两次输入密码不一致");
+
+        //2.数据库检测账号是否存在
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        long count = this.baseMapper.selectCount(queryWrapper);
+        if (count > 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号重复");
+        }
+        //3.加密数据
+        String encryptPassword = getEncryptPassword(password);
+        //4.将加密的数据存入数据库
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        user.setUserName("无名");
+        user.setUserRole(UserRoleEnum.USER.getValue());
+        boolean saveResult = this.save(user);
+        if (!saveResult) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "注册失败");
+        }
+        //5.返回用户id
+        return user.getId();
+
+    }
+
+    @Override
+    public LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        //校验
+        if (StrUtil.hasBlank(userAccount, userPassword))
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数不能为空");
+        if (userAccount.length() < 6 || userAccount.length() > 16)
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号长度必须在6-16位之间");
+        if (userPassword.length() < 6 || userPassword.length() > 16)
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码长度必须在6-16位之间");
+        //用户传递密码加密
+        String encryptPassword = getEncryptPassword(userPassword);
+        //查询数据是否存在
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount).eq("userPassword", encryptPassword);
+        User user = this.baseMapper.selectOne(queryWrapper);
+        //不存在抛出异常
+        if (user == null)
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR, "用户不存在或密码错误");
+        //正确则返回用户信息
+        request.getSession().setAttribute(USER_LOGIN_STATE, user);
+        return this.getLoginUserVO(user);
+    }
+
+    @Override
+    public LoginUserVO getLoginUserVO(User user) {
+        LoginUserVO loginUserVO = new LoginUserVO();
+        BeanUtil.copyProperties(user, loginUserVO);
+        return loginUserVO;
+    }
+
+    @Override
+    public User getLoginUser(HttpServletRequest request) {
+        // 先判断是否已登录
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        // 从数据库查询（追求性能的话可以注释，直接返回上述结果）
+        long userId = currentUser.getId();
+        currentUser = this.getById(userId);
+        if (currentUser == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+
+    @Override
+    public boolean userLogout(HttpServletRequest request) {
+        // 先判断是否已登录
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        if (userObj == null) {
+            throw new BusinessException(ErrorCode.OPERATION_ERROR, "未登录");
+        }
+        // 移除登录态
+        request.getSession().removeAttribute(USER_LOGIN_STATE);
+        return true;
+    }
+
+    @Override
+    public UserVO getUserVO(User user) {
+        if (user == null) {
+            return null;
+        }
+        UserVO userVO = new UserVO();
+        BeanUtil.copyProperties(user, userVO);
+        return userVO;
+    }
+
+    @Override
+    public List<UserVO> getUserVOList(List<User> userList) {
+        if (CollUtil.isEmpty(userList)) {
+            return new ArrayList<>();
+        }
+        return userList.stream().map(this::getUserVO).collect(Collectors.toList());
+    }
+
+    @Override
+    public QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest) {
+        if (userQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请求参数为空");
+        }
+        Long id = userQueryRequest.getId();
+        String userAccount = userQueryRequest.getUserAccount();
+        String userName = userQueryRequest.getUserName();
+        String userProfile = userQueryRequest.getUserProfile();
+        String userRole = userQueryRequest.getUserRole();
+        String sortField = userQueryRequest.getSortField();
+        String sortOrder = userQueryRequest.getSortOrder();
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq(ObjUtil.isNotNull(id), "id", id);
+        queryWrapper.eq(StrUtil.isNotBlank(userRole), "userRole", userRole);
+        queryWrapper.like(StrUtil.isNotBlank(userAccount), "userAccount", userAccount);
+        queryWrapper.like(StrUtil.isNotBlank(userName), "userName", userName);
+        queryWrapper.like(StrUtil.isNotBlank(userProfile), "userProfile", userProfile);
+        queryWrapper.orderBy(StrUtil.isNotEmpty(sortField), sortOrder.equals("ascend"), sortField);
+        return queryWrapper;
+    }
+
+    @Override
+    public String getEncryptPassword(String userPassword) {
+        // 盐值，混淆密码
+        final String SALT = "domye";
+        return DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+    }
 }
 
 
