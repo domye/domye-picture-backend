7601d361a91de1e46e6ae51460087329510f8fd7
Domye <a1523610551@163.com>
2025-10-28 00:36:37 +0800

    更新Elasticsearch依赖版本并优化客户端配置

--- src/main/java/com/domye/picture/config/ElasticsearchConfig.java
+++ src/main/java/com/domye/picture/config/ElasticsearchConfig.java
@@ -1,6 +1,5 @@
 package com.domye.picture.config;
 
-
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
@@ -61,41 +60,38 @@ public class ElasticsearchConfig {
 
 
     /**
-     * 创建Elasticsearch客户端
+     * 如果@Bean没有指定bean的名称，那么这个bean的名称就是方法名
      */
     @Bean(name = "restHighLevelClient")
     public RestHighLevelClient restHighLevelClient() {
-        try {
-            // 此处为单节点es
-            String host = hosts.split(":")[0];
-            String port = hosts.split(":")[1];
-            HttpHost httpHost = new HttpHost(host, Integer.parseInt(port), scheme);
 
-            // 设置用户名、密码
-            CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY,
-                    new UsernamePasswordCredentials(userName, password));
+        // 此处为单节点es
+        String host = hosts.split(":")[0];
+        String port = hosts.split(":")[1];
+        HttpHost httpHost = new HttpHost(host, Integer.parseInt(port));
 
-            // 构建连接对象
-            RestClientBuilder builder = RestClient.builder(httpHost)
-                    .setHttpClientConfigCallback(httpClientBuilder -> {
-                        httpClientBuilder.setMaxConnTotal(maxConnectNum);
-                        httpClientBuilder.setMaxConnPerRoute(maxConnectNumPerRoute);
-                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-                        return httpClientBuilder;
-                    })
-                    .setRequestConfigCallback(requestConfigBuilder -> {
-                        requestConfigBuilder.setConnectTimeout(connectTimeOut);
-                        requestConfigBuilder.setSocketTimeout(socketTimeOut);
-                        requestConfigBuilder.setConnectionRequestTimeout(connectionRequestTimeOut);
-                        return requestConfigBuilder;
-                    });
-            log.info("成功");
-            // 创建高级客户端
-            return new RestHighLevelClient(builder);
-        } catch (Exception e) {
-            log.error("Elasticsearch客户端创建失败", e);
-            throw new RuntimeException("Elasticsearch客户端创建失败", e);
-        }
+        // 构建连接对象
+        RestClientBuilder builder = RestClient.builder(httpHost);
+
+        // 设置用户名、密码
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+
+        // 连接延时配置
+        builder.setRequestConfigCallback(requestConfigBuilder -> {
+            requestConfigBuilder.setConnectTimeout(connectTimeOut);
+            requestConfigBuilder.setSocketTimeout(socketTimeOut);
+            requestConfigBuilder.setConnectionRequestTimeout(connectionRequestTimeOut);
+            return requestConfigBuilder;
+        });
+        // 连接数配置
+        builder.setHttpClientConfigCallback(httpClientBuilder -> {
+            httpClientBuilder.setMaxConnTotal(maxConnectNum);
+            httpClientBuilder.setMaxConnPerRoute(maxConnectNumPerRoute);
+            httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+            return httpClientBuilder;
+        });
+
+        return new RestHighLevelClient(builder);
     }
 }
